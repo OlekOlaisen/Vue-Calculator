60401736669b4ff1f3cd4eed5e37f8ed5e6e6c75
olekristian1993 <69903623+olekristian1993@users.noreply.github.com>
2022-11-02 15:57:44 +0100

    Changed style on calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,26 +1,31 @@
 <template>
-   <div class="calculator">
-      <h1>Calculator</h1>
-      <p class="calculator__input">0</p>
-      <div class="calculator__operators">
-         <button class="operator">/</button>
-         <button class="operator">x</button>
-         <button class="operator">-</button>
-         <button class="operator">+</button>
-         <button class="operator">CE</button>
-      </div>
-      <div class="calculator__buttons">
-         <button class="button">1</button>
-         <button class="button">2</button>
-         <button class="button">3</button>
-         <button class="button">4</button>
-         <button class="button">5</button>
-         <button class="button">6</button>
-         <button class="button">7</button>
-         <button class="button">8</button>
-         <button class="button">9</button>
-         <button class="button">0</button>
-         <button class="operator">=</button>
+   <div class="container">
+
+      <div class="calculator">
+         <h1>Calculator</h1>
+         <div class="calculator-output">
+            <p class="calculator-output__numbers">0</p>
+         </div>
+
+         <div class="calculator-buttons">
+            <button class="button span-3 operator">CE</button>
+            <button class="button operator">&divide;</button>
+            <button class="button">7</button>
+            <button class="button">8</button>
+            <button class="button">9</button>
+            <button class="button operator">x</button>
+            <button class="button">4</button>
+            <button class="button">5</button>
+            <button class="button">6</button>
+            <button class="button operator">-</button>
+            <button class="button">1</button>
+            <button class="button">2</button>
+            <button class="button">3</button>
+            <button class="button operator">+</button>
+            <button class="button span-3">0</button>
+            <button class="button operator">=</button>
+         </div>
+
       </div>
 
    </div>
@@ -30,82 +35,101 @@
 
 </script>
 
-<style scoped>
-* {
-   margin: 0;
-   padding: 0;
-   box-sizing: border-box;
+<style>
+:root {
+
+   --background: #344050;
+   --background-operators: #404D5E;
+   --background-output__numbers: #425062;
 }
 
+
 h1 {
-   font-weight: 500;
-   font-family: Arial, Helvetica, sans-serif;
    font-size: 1rem;
-   margin-left: 1rem;
-   margin-top: 1rem;
+   color: white;
+   margin: 0.5rem;
+   font-family: Arial, Helvetica, sans-serif;
+   text-align: center;
+
 }
 
 .calculator {
+   max-width: 320px;
+   background: var(--background);
    width: 320px;
    height: 530px;
    border: 1px solid black;
-   margin: 2rem auto;
-   border-radius: 1rem;
-}
-
-.calculator__input {
-   width: 90%;
-   height: 7rem;
    margin: 1rem auto;
    border-radius: 1rem;
-   border: 1px solid black;
-   font-size: 2rem;
+}
+
+.calculator-output {
    font-family: Arial, Helvetica, sans-serif;
-}
-
-.calculator__buttons {
-   display: grid;
-   grid-template-columns: repeat(3, 1fr);
+   font-size: 3rem;
+   padding: 0.3rem;
+   text-align: right;
+   height: 100px;
    margin: 1rem;
-   gap: 0.5rem;
-   grid-column-end: span 1;
-}
-
-.calculator__operators {
-   display: flex;
-   gap: 0.5rem;
-   justify-content: center;
-   margin: 3rem 1rem 1rem;
-}
-
-.operator {
-   border-radius: 0.3rem;
-   border: none;
-   padding: 1rem;
-   width: 100%;
-   background-color: grey;
    color: white;
 }
 
-.operator:hover {
-   background-color: rgb(80, 80, 80);
+.calculator-buttons {
+   output: grid;
+   grid-template-columns: repeat(4, 1fr);
+   border: 0;
+   gap: 0.2rem;
+   margin: 2rem 1rem;
+
 }
 
+.span-2 {
+   grid-column: span 2;
+}
+
+.span-3 {
+   grid-column: span 3;
+}
 
 .button {
-   border-radius: 0.3rem;
+   font-size: 1rem;
+   height: 65px;
    border: none;
-   padding: 1rem;
-   background-color: grey;
-   color: white;
-}
-
-.button:nth-child(10) {
-   background-color: lightgreen;
-   grid-column: 1 / span 2;
+   color: #fff;
+   background-color: var(--background-output__numbers);
+   border-radius: 0.3rem;
 }
 
 .button:hover {
-   background-color: rgb(80, 80, 80);
+   background: #627896;
+}
+
+
+.button:nth-child(1) {
+   color: red;
+   font-size: 1.2rem;
+}
+
+.button:nth-child(1):hover {
+   background-color: rgb(240, 92, 92);
+   font-size: 1.2rem;
+   color: white;
+}
+
+.button:nth-child(16) {
+   color: lightgreen;
+   font-size: 1.2rem;
+}
+
+.button:nth-child(16):hover {
+   background-color: lightgreen;
+   font-size: 1.2rem;
+   color: white;
+}
+
+
+.operator {
+   background-color: #404D5E;
+   color: #A8ADB5;
+   font-size: 1.5rem;
 }
 </style>
